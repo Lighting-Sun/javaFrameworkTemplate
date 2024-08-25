5d78d1a54d2ab3f0e975955edf33429c163fa473
Harvey C <harveydavid14@gmail.com>
2024-08-25 18:52:24 -0500

    Added driver singleton to make sure that no more than one instance of the driver gets instantiated

---
+++ src/main/java/org/example/driver/DriverSingleton.java
@@ -0,0 +1,55 @@
+package org.example.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Singleton class to manage the WebDriver instance.
+ * @author Harvey C
+ */
+public class DriverSingleton {
+
+    private static WebDriver driver;
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private DriverSingleton() {}
+
+    /**
+     * Gets the single instance of WebDriver.
+     * Initializes the WebDriver if it is not already initialized.
+     *
+     * @return the WebDriver instance
+     */
+    public static WebDriver getDriver() {
+        if (null == driver) {
+            switch (System.getProperty("browser")) {
+                case "firefox": {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                }
+                default: {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                }
+            }
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    /**
+     * Closes the WebDriver instance and sets it to null.
+     */
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
